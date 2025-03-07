df703094439d17ad63c7ec6bfc4d173e1a290909
Miguel Aguirre <m.a.a.f1999@hotmail.com>
2025-03-07 16:10:18 -0500

    actualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import partytown from '@astrojs/partytown'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations:[partytown()],
   vite: {
     plugins: [tailwindcss()]
   }
